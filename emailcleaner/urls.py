8d72afb168e1b3c348310bf6680975df661def4c
Elocodes <elochukwuodoeze@gmail.com>
2024-02-22 05:11:16 +0000

    added routes for landing page, authenticate, and move to trash

--- emailcleaner/urls.py
+++ emailcleaner/urls.py
@@ -19,5 +19,8 @@ from emails import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('authenticate-gmail/', views.authenticate_gmail, name='authenticate_gmail'),
+    path('choose-cleanup-date/', views.choose_cleanup_date, name='choose_cleanup_date'),
     path('move-to-trash/', views.move_messages_to_trash, name='move_messages_to_trash'),
 ]
